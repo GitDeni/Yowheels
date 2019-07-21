e9bbbeee91651a6e847127ac114f47d6b9032ffb
GitDeni <denidelippa@gmail.com>
2019-07-21 15:42:50 +0300

    to footer

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,13 +1,13 @@
-var hide = function(e){
-	e.preventDefault();
-	$(this).addClass('closed');
-	$(this).parents('.faq-card').children('.faq-card__answer').slideUp();
+// var hide = function(e){
+// 	e.preventDefault();
+// 	$(this).addClass('closed');
+// 	$(this).parents('.faq-card').children('.faq-card__answer').slideUp();
 
-};
-var show = function(e){
-	e.preventDefault();
-	$(this).removeClass('closed');
-	$(this).parents('.faq-card').children('.faq-card__answer').slideDown();
-}
-$('body').on('click','.faq-button', hide);
-$('body').on('click', '.closed', show);
+// };
+// var show = function(e){
+// 	e.preventDefault();
+// 	$(this).removeClass('closed');
+// 	$(this).parents('.faq-card').children('.faq-card__answer').slideDown();
+// }
+// $('body').on('click','.faq-button', hide);
+// $('body').on('click', '.closed', show);
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import './vendor';
 import './anchor-links';
 import './faq';
+import './map';
